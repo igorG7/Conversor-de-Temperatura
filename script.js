705bb8460c3379825e4b157ba8e0604abfe8b95a
igorG7 <giovane167@outlook.com>
2022-03-24 18:53:02 -0300

    remoção de comentários

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function calculo(){
-    var valorDeEntrada = document.getElementById("valorEntrada").value; // recebe o valor do input "insira o valor"
+    var valorDeEntrada = document.getElementById("valorEntrada").value; 
 
-    var medidaDeEntradaInicial = document.getElementById('medidaDeEntrada').value; // recebe o valor do <option> da tag <select>
-    var medidaConvertidaRetorno = document.getElementById('medidaConvertida').value; // recebe o valor do <option> da tag <select>
+    var medidaDeEntradaInicial = document.getElementById('medidaDeEntrada').value; 
+    var medidaConvertidaRetorno = document.getElementById('medidaConvertida').value;
 
     console.log(valorDeEntrada);
     //Medida de entrada = Celsius 
@@ -14,7 +14,7 @@ function calculo(){
         if (medidaConvertidaRetorno == "kelvin") {
             var operacao = parseFloat(valorDeEntrada) + 273.15;
         } 
-        document.getElementById("resultado").value = operacao.toFixed(2) //Retorna o valor da variavel operacao no elemento de input "resultado"
+        document.getElementById("resultado").value = operacao.toFixed(2) 
       }
       //Medida de entrada = Fahrenheit
       if (medidaDeEntradaInicial == "fahrenheit") {
@@ -24,7 +24,7 @@ function calculo(){
           if(medidaConvertidaRetorno == "kelvin"){
               var operacao = (valorDeEntrada - 32) * 5/9 + 273.15;
           }
-          document.getElementById("resultado").value = operacao.toFixed(2) //Retorna o valor da variavel operacao no elemento de input "resultado"
+          document.getElementById("resultado").value = operacao.toFixed(2) 
       }
       //Medida entrada = Kelvin
       if (medidaDeEntradaInicial == "kelvin"){
@@ -34,7 +34,7 @@ function calculo(){
           if(medidaConvertidaRetorno == "fahrenheit"){
               var operacao = (valorDeEntrada - 273.15) * 9/5 + 32;
           }
-          document.getElementById("resultado").value = operacao.toFixed(2) //Retorna o valor da variavel operacao no elemento de input "resultado"
+          document.getElementById("resultado").value = operacao.toFixed(2) 
       }
                          
       console.log(operacao);
